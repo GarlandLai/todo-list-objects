3eee8dc1c68289e35927253fb16c9267e4865a8e
Garland Lai and Josh Mabry <garland@>
2019-06-17 09:59:42 -0700

    add logic to push user input to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function TodoList() {
   this.tasks = [];
+  this.currentId = 0;
 }
 
 function Task(item, location) {
@@ -8,7 +9,13 @@ function Task(item, location) {
 }
 
 TodoList.prototype.addTask = function (task) {
+  task.id = this.assignId();
   this.tasks.push(task);
+};
+
+TodoList.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
 };
 
 var todoListPersonal = new TodoList();
@@ -16,3 +23,13 @@ var todoListPersonal = new TodoList();
 var task1 = new Task('walk the dog', 'home');
 
 todoListPersonal.addTask(task1);
+
+//frontend logic
+
+$(document).ready(function () {
+  $('#formOne').submit(function (event) {
+    event.preventDefault();
+    var input = $('.input').val();
+    task = new Task(input, '');
+  });
+});
